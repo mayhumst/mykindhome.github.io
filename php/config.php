612d9337186547e27bcee09b11fe08954ca6b4e5
mayhumst <91208973+mayhumst@users.noreply.github.com>
2023-04-17 17:38:50 -0400

    remove database specifications

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php //establishes connection to mysql server
 
 $db_user = 'root';
-$db_pass = 'MidnightV0lc@n0s';
+$db_pass = '';
 $db_name = "mykindhome";
 
 $db = new PDO('mysql:host = localhost;dbname='. $db_name .'; charset=utf8', $db_user, $db_pass);
